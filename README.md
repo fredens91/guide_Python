29fba5e36b8476356add71d92cf755d0c5a19c9d
fedeorland <orlandifederico91@gmail.com>
2024-05-06 11:37:02 +0200

    comm

--- README.md
+++ README.md
@@ -6,23 +6,49 @@ Installa Python su Windows.
 
 ### VS Code Plugins
 - Python (estensione ufficiale di Microsoft)
-- Kivy (estensione per il framewokr Kivy)
+- Kivy (estensione per il framework Kivy)
 
 ## Sintassi
 
-### Variabili
-int
+### Variabili e Classi
 
-`numero = 10`
+# numeri
+`numInt = 10`
+`numFloat = 5.4`
 
 stringa
-
 `nome = "Alice"`
 
-lista
+boolean
+`value = True`
 
+lista
 `lista = [1, 2, 3, 4, 5]`
 
 dizionario
-
 `dizionario = {"nome": "Bob", "età": 25}`
+
+Classi
+
+```
+class Persona
+    def __init__(self, nome, età):
+        self.nome = nome
+        self.età = età
+    
+    def saluta(self):
+        print(f"Ciao, mi chiamo {self.nome} e ho {self.età} anni.")
+
+# Creazione di un nuovo oggetto Persona
+persona1 = Persona("Alice", 30)
+
+# Accesso agli attributi dell'oggetto
+print(persona1.nome)  # Stampa: Alice
+print(persona1.età)   # Stampa: 30
+
+# Chiamata a un metodo dell'oggetto
+persona1.saluta()     # Stampa: Ciao, mi chiamo Alice e ho 30 anni.
+```
+
+
+### Operazioni
--- main.py
+++ main.py
@@ -8,16 +8,96 @@ print("Hello World")
 
 ## variabili
 
-# variabile int
-numero = 10
+# numeri
+numInt = 10
+numFloat = 5.4
 
-# variabile stringa
+# stringa
 nome = "Alice"
 
-# Definizione di una variabile lista
+#boolean
+boolean = True
+
+# list
 lista = [1, 2, 3, 4, 5]
 
-# Definizione di una variabile dizionario
+# dizionario
 dizionario = {"nome": "Bob", "età": 25}
+
+
+# Classe
+class Persona:
+    def __init__(self, nome, età):
+        self.nome = nome
+        self.età = età
+    
+    def saluta(self):
+        print(f"Ciao, mi chiamo {self.nome} e ho {self.età} anni.")
+
+# Creazione di un nuovo oggetto Persona
+persona1 = Persona("Alice", 30)
+
+# Accesso agli attributi dell'oggetto
+print(persona1.nome)  # Stampa: Alice
+print(persona1.età)   # Stampa: 30
+
+# Chiamata a un metodo dell'oggetto
+persona1.saluta()     # Stampa: Ciao, mi chiamo Alice e ho 30 anni.
+
+
+
+
+
+
+
+
+
+## Operazioni
+
+# IF
+
+if(numInt < 15):
+    print("numero minore di 15")
+else:
+    print("numero maggiore di 15")
+
+# Switch
+
+# Non esiste lo Switch Case ma si può creare una funzione
+def switch_case(argument):
+    switcher = {
+        1: "Primo caso",
+        2: "Secondo caso",
+        3: "Terzo caso"
+        # Aggiungere altri casi se necessario
+    }
+    
+    # Ottenere il valore corrispondente all'argomento
+    risultato = switcher.get(argument, "Caso predefinito")
+    
+    # Restituire il risultato
+    return risultato
+
+print(switch_case(1))  # Stampa: Primo caso
+print(switch_case(2))  # Stampa: Secondo caso
+print(switch_case(4))  # Stampa: Caso predefinito
+
+
+# for (non esiste il foreach)
+
+# Iterazione attraverso una lista
+numeri = [1, 2, 3, 4, 5]
+for numero in numeri:
+    print(numero)
+
+# Iterazione attraverso una stringa
+stringa = "Python"
+for carattere in stringa:
+    print(carattere)
+
+# Iterazione attraverso un dizionario
+dizionario = {"chiave1": 1, "chiave2": 2, "chiave3": 3}
+for chiave, valore in dizionario.items():
+    print(chiave, valore)
 
 
